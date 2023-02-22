1ac3483dff78c0b51a65bebbda6418802dbf2f1b
Abhay Singh <abhay.jhonsmith@gmail.com>
2023-02-22 20:58:36 +0530

    Minor fixes

---
+++ client/dist/assets/index-77a7be3f.js
@@ -0,0 +1,16 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=l(e);fetch(e.href,r)}})();document.querySelector("#navbar").innerHTML=`
+<nav>
+        <label class="logo"><img src="/images/logo.png" alt="logo"></label>
+        <ul>
+          <li><a href="">Pit Stop</a></li>
+          <li><a href="">Updates</a></li>
+          <li><a href="">Discord</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Merch</a></li>
+          <li><a href="">CREATE ACCOUNT</a></li>
+          <li><a href="">SIGN IN</a></li>
+        </ul>
+        <div id="user-wpm-nav">0 WPM</div>
+        <div id="user-race-nav">0 RACES</div>
+      </nav>
+`;document.querySelector("#latest");document.querySelector("#my-scores");document.querySelector("#hof");

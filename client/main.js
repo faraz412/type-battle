9e3ad3cf65a8a4ef6555015daec13348b34e5536
Abhay Singh <abhay.jhonsmith@gmail.com>
2023-02-22 20:45:53 +0530

    Added Nav

--- client/main.js
+++ client/main.js
@@ -1,10 +1,21 @@
 import './style.css'
 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-  <h1>Type Battle</h1>
-  </div>
+document.querySelector('#navbar').innerHTML = `
+<nav>
+        <label class="logo"><img src="/images/logo.png" alt="logo"></label>
+        <ul>
+          <li><a href="">Pit Stop</a></li>
+          <li><a href="">Updates</a></li>
+          <li><a href="">Discord</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Merch</a></li>
+          <li><a href="">CREATE ACCOUNT</a></li>
+          <li><a href="">SIGN IN</a></li>
+        </ul>
+        <div id="user-wpm-nav">0 WPM</div>
+        <div id="user-race-nav">0 RACES</div>
+      </nav>
 `
 
-setupCounter(document.querySelector('#counter'))
+

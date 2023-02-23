18bfb3873d10b14817ac4a702a6dca42caddd083
Abhay Singh <abhay.jhonsmith@gmail.com>
2023-02-23 16:32:47 +0530

    Build|Added Admin Side|Added Socket

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const connection = require("./config/db")
 require("dotenv").config()
 const cors=require("cors")
 const path=require("path")
-const userrouter = require("./router/userrouter")
-const adminrouter = require("./router/adminrouter")
 app.use(cors())
 app.use(express.json())
-console.log(process.env.PORT)
+
 
 app.get("/",(req,res)=>{
     app.use(express.static(path.join(__dirname,"client","dist")))
@@ -18,15 +20,23 @@ app.get("/",(req,res)=>{
 app.get("/api",(req,res)=>{
     res.send({"msg":"welcome"})
 })
-app.use("/user",userrouter);
-app.use("/admin",adminrouter);
+app.use("/api/user",require("./router/userrouter"));
+app.use("/api/admin",require("./router/adminrouter"));
 
-app.listen(process.env.PORT,async ()=>{
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  
+  server.listen(process.env.PORT,async () => {
     try {
         await connection
         console.log("db connection established");
     } catch (error) {
         console.log(error.message,"not connected");
     }
-    console.log("listening on port"+ process.env.PORT);
-})
+    console.log('listening on *:'+ process.env.PORT);
+  });
+

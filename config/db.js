fe4b5ff2abcc036189de4d9d80ffe4d3a4a05d4a
Abhay Singh <55654179+DanteHaxor@users.noreply.github.com>
2023-04-11 10:15:22 +0530

    Removed Mongo Atlas URL

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
-const connection =  mongoose.connect(process.env.mongourl || "mongodb+srv://dantehaxor:dantehaxor@cluster0.6eerxwr.mongodb.net/?retryWrites=true&w=majority")
+const connection =  mongoose.connect(process.env.mongourl)
 module.exports = connection
